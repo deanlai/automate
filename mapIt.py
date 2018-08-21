2e3813c1864837617a855e81c9522e78df842661
Sean Lai <sean.j.lai@gmail.com>
2018-08-20 20:01:52 -0700

    Added mapIt and Shruggie, two very useful scripts

---
+++ mapIt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# mapIt.py - Launches a map in the browser using an address from
+# the command line or clipboard
+
+# Usage: mapIt.py <address> to open a Google Maps browser window for
+# the given address, or from the address in the clipboard if given no arguments
+
+import webbrowser
+import sys
+import pyperclip
+
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
---
+++ shruggie.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Puts a ¯\_(ツ)_/¯ in the clipboard
+
+import pyperclip
+
+pyperclip.copy('¯\_(ツ)_/¯')
